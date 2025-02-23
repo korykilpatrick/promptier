6897221e8ba8d0dedaf0ea3d395aac9f731b8ff1
korykilpatrick <koryrkilpatrick@gmail.com>
2025-02-23 14:42:33 -0600

    removed popup references

--- extension/src/sidepanel.tsx
+++ extension/src/sidepanel.tsx
@@ -21,11 +21,7 @@ function SidePanel() {
       signInFallbackRedirectUrl={chrome.runtime.getURL("sidepanel.html")}
       signUpFallbackRedirectUrl={chrome.runtime.getURL("sidepanel.html")}
     >
-      <div 
-        className="plasmo-h-screen plasmo-w-[480px] plasmo-bg-gray-50 plasmo-flex plasmo-flex-col"
-        role="complementary"
-        aria-label="Promptier Side Panel"
-      >
+      <div className="plasmo-h-full plasmo-w-[480px] plasmo-bg-gray-50 plasmo-flex plasmo-flex-col">
         <header className="plasmo-bg-white plasmo-p-4 plasmo-shadow-sleek plasmo-flex plasmo-justify-between plasmo-items-center">
           <h1 className="plasmo-text-xl plasmo-font-semibold plasmo-text-gray-900">Promptier</h1>
           <SignedIn>
@@ -37,7 +33,7 @@ function SidePanel() {
             <Sidebar />
           </SignedIn>
           <SignedOut>
-            <div className="plasmo-p-6 plasmo-flex plasmo-flex-col plasmo-items-center">
+            <div className="plasmo-p-6">
               <h2 className="plasmo-text-lg plasmo-font-medium plasmo-mb-4">Sign In to Promptier</h2>
               <SignIn 
                 routing="virtual"
