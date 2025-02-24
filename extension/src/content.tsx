4a1f06e092e7c9473e34afb31926dc3e68db87e9
korykilpatrick <koryrkilpatrick@gmail.com>
2025-02-23 18:51:55 -0600

    got rid of count button

--- extension/src/content.tsx
+++ extension/src/content.tsx
@@ -1,29 +1,21 @@
 import cssText from "data-text:~style.css";
 import type { PlasmoCSConfig } from "plasmo";
-import { CountButton } from "~features/count-button";
 
 export const config: PlasmoCSConfig = {
   matches: ["<all_urls>"],
 };
 
-export const getStyle = (): HTMLStyleElement => {
-  const baseFontSize = 16;
-  let updatedCssText = cssText.replaceAll(":root", ":host(plasmo-csui)");
-  const remRegex = /([\d.]+)rem/g;
-  updatedCssText = updatedCssText.replace(remRegex, (match, remValue) => {
-    const pixelsValue = parseFloat(remValue) * baseFontSize;
-    return `${pixelsValue}px`;
-  });
-
-  const styleElement = document.createElement("style");
-  styleElement.textContent = updatedCssText;
-  return styleElement;
+export const getStyle = () => {
+  const style = document.createElement("style");
+  style.textContent = cssText;
+  return style;
 };
+
+export const getShadowHostId = () => "plasmo-shadow-host";
 
 const PlasmoOverlay = () => {
   return (
     <div className="plasmo-z-50 plasmo-flex plasmo-fixed plasmo-top-32 plasmo-right-8">
-      <CountButton />
     </div>
   );
 };
